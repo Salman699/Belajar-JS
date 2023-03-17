9e802a67e18174103ba3d55d984d3b45e47496d9
Salman699 <salmantubagus43@gmail.com>
2023-03-17 19:07:32 +0700

    jquery

--- latJs/Js/kalkulator.js
+++ latJs/Js/kalkulator.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function tambah(){
     var frm = document.getElementById('kalkulator');
     var a1 = parseFloat(frm.angka.value);
@@ -13,20 +12,12 @@ function kurang(){
     var a2 = parseFloat(frm.angka2.value);
     var total = a1 - a2;
     frm.hasil.value = total;
-=======
-function tambah(){
-    var frm = document.getElementById('kalkulator');
-    var a1 = parseFloat(frm.angka.value);
-    var a2 = parseFloat(frm.angka2.value);
-    var total = a1 + a2;
-    frm.hasil.value = total;
 }
-
-function kurang(){
-    var frm = document.getElementById('kalkulator');
-    var a1 = parseFloat(frm.angka.value);
-    var a2 = parseFloat(frm.angka2.value);
-    var total = a1 - a2;
-    frm.hasil.value = total;
->>>>>>> 8edcdb507a04013c97692bf0cda76f06207a37d4
-}
+1. Lengkapilah form diatas menggunakan CSS dan table 
+2. Perbagus tampilannya 
+3. Lengkapi perhitungannya dengan:
+- pembagian
+- perkalian
+- pangkat
+4. jika form input dari angka 1 dan 2 kosong dan user klik button, 
+maka tampilkan "Maaf angka belum terisi"
